5ee3c22661c4cc0cc252994d4da374e1d03a9101
Maria Antici <maria.antici99@gmail.com>
2022-12-26 12:33:44 +0200

    Setting basic styling

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brown-black': '#282120',
+        'milky-white': '#f9f9f9'
+      },
+    },
+  },
+  plugins: [],
+}
