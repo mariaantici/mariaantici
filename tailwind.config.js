81ef494a795a190112ef149edffe15fd76fda49d
Maria Antici <maria.antici99@gmail.com>
2022-12-27 10:18:06 +0200

    Customizing Screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
         'milky-white': '#f9f9f9'
       },
     },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      'sides': '900px'
+    },
   },
   plugins: [],
 }
