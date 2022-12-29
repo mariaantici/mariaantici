823fdc5aa85826fe26ae02ef29f28f41b590c1a3
Maria Antici <maria.antici99@gmail.com>
2022-12-29 10:13:12 +0200

    Styling for Socials text for icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -8,18 +8,22 @@ import {
 
 const socials = [
     {
+        name: "Email me!",
         icon: faEnvelope,
         url: "mailto: maria.antici99@mail.com",
     },
     {
+        name: "Github",
         icon: faGithub,
         url: "https://github.com/mariaantici",
     },
     {
+        name: "LinkedIn",
         icon: faLinkedinIn,
         url: "https://www.linkedin.com/in/maria-antici",
     },
     {
+        name: "Instagram",
         icon: faInstagram,
         url: "https://www.instagram.com/maria_antici",
     },
@@ -31,7 +35,7 @@ export default function Socials() {
             <div className="px-10 space-y-4">
                 <div className="flex flex-col space-y-4">
                     {socials.map((social) => (
-                        <a href={social.url} className='icon transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300'><FontAwesomeIcon icon={social.icon} size="2x" /></a>
+                        <><a href={social.url} className='icon transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300 socialIcon'><FontAwesomeIcon icon={social.icon} size="2x" /></a><span className="socialName">{social.name}</span></>
                     ))}
                 </div>
                 <div className="flex flex-row justify-center">
