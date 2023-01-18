15e4b1fc8cf12de584db6b7b17d39ad717b815e5
Maria Antici <maria.antici99@gmail.com>
2023-01-18 13:21:43 +0200

    App.js components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Homepage from './components/Homepage';
+import AboutSection from './components/AboutSection';
+import ProjectsSection from './components/ProjectsSection';
+import ExperienceSection from './components/ExperienceSection';
+import ContactSection from './components/ContactSection';
+import Footer from './components/Footer';
+import SideNavbar from './components/SideNavbar';
+import Socials from './components/Socials';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Homepage />
+        <AboutSection />
+        <ProjectsSection />
+        <ExperienceSection />
+        <ContactSection />
+      </main>
+      <Footer />
+      <Socials />
+      <SideNavbar />
+    </>
   );
 }
 
