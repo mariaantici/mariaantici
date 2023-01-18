6ee836c8f9a73d2f8acecc5ff47a68830fd6df01
Maria Antici <maria.antici99@gmail.com>
2023-01-18 13:21:23 +0200

    Creating plain & simple Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    return (
+        <footer class="pt-14 pb-16">
+            <span class="block text-sm text-brown-black/60 text-center tracking-widest">Designed & Built by <a href="#homepage" class="hover:underline">Maria Antici</a>
+            </span>
+        </footer>
+    );
+}
