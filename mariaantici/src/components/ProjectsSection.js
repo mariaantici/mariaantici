b445be6754d0933df050e53a28837a31af181b19
Maria Antici <maria.antici99@gmail.com>
2023-02-27 13:16:47 +0200

    Adding "Work in progress" - message

--- mariaantici/src/components/ProjectsSection.js
+++ mariaantici/src/components/ProjectsSection.js
@@ -1,8 +1,8 @@
 
 
 const projects = [{
-    label: "Game of Thrones",
-    title: "A Clash of Kings",
+    label: "Work in progress",
+    title: "Coming soon",
     details: "Lorem ipsum dolor amet you probably haven't heard of them bitters selvage listicle heirloom. Locavore kombucha street art ennui 90's, organic food truck hell of seitan skateboard literally hexagon fixie next level. Lomo salvia yuccie hella roof party echo park vegan four dollar toast cred.",
     img: "https://cdn.vox-cdn.com/thumbor/w-IFN0FWpN4BGfhZaV9EYqs4nLo=/51x0:977x617/1200x800/filters:focal(51x0:977x617)/cdn.vox-cdn.com/uploads/chorus_image/image/50017015/Screen_Shot_2016-07-04_at_12.37.15_PM.0.0.png",
     tech: ['Shadow', 'Demon', 'Baby']
