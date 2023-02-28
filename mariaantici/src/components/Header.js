e9346dc7300ba38146afc00db75772dc2b519a4e
Maria Antici <maria.antici99@gmail.com>
2023-02-28 14:39:47 +0200

    Fixing the mobile menu visibility

--- mariaantici/src/components/Header.js
+++ mariaantici/src/components/Header.js
@@ -1,39 +1,56 @@
-import { Menu, Transition } from '@headlessui/react'
-import { faBars, faX } from '@fortawesome/free-solid-svg-icons'
+import { Menu, Transition } from '@headlessui/react';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState, useEffect } from 'react';
 
 const navigation = [
     { name: 'About', href: '#about', current: true },
     { name: 'Projects', href: '#projects', current: false },
     { name: 'Experience', href: '#experience', current: false },
     { name: 'Contact', href: '#contact', current: false },
-]
+];
 
 export default function Header() {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
     const handleClick = (anchor) => () => {
         const id = `${anchor}-section`;
         const element = document.getElementById(id);
         if (element) {
             element.scrollIntoView({
-                behavior: "smooth",
-                block: "start",
+                behavior: 'smooth',
+                block: 'start',
             });
         }
     };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollPos = window.pageYOffset;
+            const isVisible = prevScrollPos > currentScrollPos || currentScrollPos < 10;
+            setVisible(isVisible);
+            setPrevScrollPos(currentScrollPos);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [prevScrollPos]);
 
     return (
         <navigation>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 sm:py-6 lg:px-30 lg:py-10" id="homepage-section">
                 <div className="relative flex h-16 items-center justify-between">
-                    <div className="fixed top-10 left-10 sm:hidden z-[99999]">
+                    <div className={`fixed top-10 left-10 sm:hidden z-[99999] ${visible ? 'opacity-100' : 'opacity-0'}`}>
                         <Menu>
                             {({ open }) => (
-                                <div className=''>
-                                    <Menu.Button className='icon'>
+                                <div className="">
+                                    <Menu.Button className="icon px-4 py-3 rounded-3xl bg-milky-white ring-1 ring-brown-black ring-opacity-10">
                                         {open ? (
-                                            <FontAwesomeIcon icon={faX} size='2x' />
+                                            <FontAwesomeIcon icon={faX} size="2x" />
                                         ) : (
-                                            <FontAwesomeIcon icon={faBars} size='2x' />
+                                            <FontAwesomeIcon icon={faBars} size="2x" />
                                         )}
                                     </Menu.Button>
                                     <Transition
@@ -44,7 +61,7 @@ export default function Header() {
                                         leaveFrom="transform scale-100 opacity-100"
                                         leaveTo="transform scale-95 opacity-0"
                                     >
-                                        <Menu.Items className='absolute left-0 mt-2 px-2 origin-top-right divide-y divide-brown-black/10 rounded-xl bg-milky-white shadow-lg ring-1 ring-brown-black ring-opacity-5'>
+                                        <Menu.Items className="absolute left-0 mt-2 px-2 origin-top-right divide-y divide-brown-black/10 rounded-xl bg-milky-white shadow-lg ring-1 ring-brown-black ring-opacity-5">
                                             {navigation.map((item) => (
                                                 <Menu.Item
                                                     as="a"
@@ -79,7 +96,6 @@ export default function Header() {
                                     href={item.href}
                                     onClick={handleClick(item.name.toLowerCase())}
                                     className='text-brown-black hover:ring-1 hover:ring-brown-black/20 px-4 py-2 rounded-3xl text-lg font-medium'
-                                    aria-current={item.current ? 'page' : undefined}
                                 >
                                     {item.name}
                                 </a>
